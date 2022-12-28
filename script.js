1f6445669a929066299cd123bbeac92b9d3d7395
ashudangi20 <ashutoshdangi20@gmail.com>
2022-12-28 09:39:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+let item = document.getElementById('item-name-input');
+let price = document.getElementById('item-price-input');
+let add = document.getElementById('add');
+let table = document.getElementById('table');
+
+function addElement() {
+    let row = document.createElement('tr');
+    let data1 = document.createElement('td');
+    let data2 = document.createElement('td');
+   data1 = item.value;
+   data2 = price.value;
+    row.append(data1);
+    row.append(data2);
+    table.append(row);
+
+    console.log(data1);
+    console.log(data2);
+}
+
+add.addEventListener('click', addElement);
